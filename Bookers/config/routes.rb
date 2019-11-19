ea552873db700fd79a8cff305ea2ffd8ad4224a8
akio5445 <yusa.world2@gmail.com>
2019-11-19 11:43:53 +0000

    [Update] Bookersgit add *

--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  
+  root to: redirect('books')
+  #GET /books/:id books#index
+#get '@book', action: :show, controller: 'books'
   resources :books
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+#get '/members', to: redirect('/users')
+#get ‘/members/:id', to: redirect('/users/%{id}')
+
+#get '/books/:id', to: 'books#book', as: 'books'
+#<%= link_to 'book Record', book_path(@book) %>
